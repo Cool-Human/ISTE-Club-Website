4ba9013fa137156e81ccf6aab663711845556756
Hari Sharan <115358626+Cool-Human@users.noreply.github.com>
2025-03-03 13:35:44 +0530

    Update SocialIcons.js

--- Front-End/Components (frontend:src:components)/SocialIcons.js
+++ Front-End/Components (frontend:src:components)/SocialIcons.js
@@ -5,16 +5,16 @@ import { FaInstagram, FaLinkedin, FaWordpress, FaYoutube } from 'react-icons/fa'
 function SocialIcons() {
   return (
     <div className="flex space-x-4">
-      <a href="https://www.instagram.com/iste_nitk/" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
+      <a href="https://www.instagram.com/istenitk/?hl=en" target="_blank" rel="noopener noreferrer" aria-label="Instagram">
         <FaInstagram size={24} />
       </a>
-      <a href="https://www.linkedin.com/company/iste-nitk-student-chapter/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
+      <a href="https://in.linkedin.com/company/istenitk" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
         <FaLinkedin size={24} />
       </a>
       <a href="https://istenitk.wordpress.com/" target="_blank" rel="noopener noreferrer" aria-label="WordPress">
         <FaWordpress size={24} />
       </a>
-      <a href="https://www.youtube.com/channel/UCz46T-kRz4eaGv2EAe4Br5g" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
+      <a href="https://www.youtube.com/@ISTENITK" target="_blank" rel="noopener noreferrer" aria-label="YouTube">
         <FaYoutube size={24} />
       </a>
     </div>
